804c961b37042809213ee58e4fb8ffb8fc97c483
Aleksandr Endovitskii <endovitsky.a.p@gmail.com>
2022-07-03 05:22:08 +0400

    Add EnumExtensions

---
+++ Assets/Scripts/Extensions/EnumExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public static class EnumExtensions
+{
+    public static T GetNextValue<T>(this T enumValue) where T : Enum
+    {
+        var values = (T[])Enum.GetValues(enumValue.GetType());
+        var currentIndex = Array.IndexOf(values, enumValue);
+        var nextIndex = currentIndex + 1;
+
+        if (nextIndex >= values.Length)
+        {
+            var firstValue = values[0];
+            return firstValue;
+        }
+
+        var nextValue = values[nextIndex];
+        return nextValue;
+    }
+}
