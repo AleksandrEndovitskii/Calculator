d92f7bfac6ab484c0ba27e2b2a0c284d427d2ce3
Aleksandr Endovitskii <endovitsky.a.p@gmail.com>
2022-07-08 01:57:36 +0400

    Add InputNumberTextComponent

---
+++ Assets/Scripts/Components/InputComponents/InputNumberTextComponent.cs
@@ -0,0 +1,40 @@
+﻿using Common;
+using Components.BaseComponents;
+using Extensions;
+using Models;
+using UnityEngine;
+using Views;
+
+namespace Components.InputComponents
+{
+    public class InputNumberTextComponent : BaseTextComponent
+    {
+        [SerializeField]
+        private InputNumberView _view;
+
+        public override void Subscribe()
+        {
+            this.InvokeActionAfterAllConditionsAreMet(() =>
+                {
+                    _view.ModelChanged += ViewViewModelChanged;
+                    ViewViewModelChanged(_view.Model);
+                },
+                () =>  _view != null);
+        }
+        public override void UnSubscribe()
+        {
+            if (_view != null)
+            {
+                _view.ModelChanged -= ViewViewModelChanged;
+            }
+        }
+
+        private void ViewViewModelChanged(IModel model)
+        {
+            var inputNumberModel = (InputNumberModel)model;
+
+            var value = inputNumberModel?.Data.ToString();
+            Redraw(value);
+        }
+    }
+}
