82c8f30f36b3cfcc8c27801d6e64db6057fd428d
Aleksandr Endovitskii <endovitsky.a.p@gmail.com>
2022-07-08 01:59:12 +0400

    Replace initialization of InputNumberCalculatorButton from component to view

--- Assets/Scripts/Components/InputComponents/InputNumberButtonComponent.cs
+++ Assets/Scripts/Components/InputComponents/InputNumberButtonComponent.cs
@@ -1,17 +1,28 @@
 using Components.BaseComponents;
 using Managers;
 using UnityEngine;
+using Views;
 
 namespace Components.InputComponents
 {
     public class InputNumberButtonComponent : BaseButtonComponent
     {
         [SerializeField]
-        private int _value;
+        private InputNumberView _view;
 
         protected override void ButtonOnClick()
         {
-            InputManager.Instance.HandleInput(_value);
+            if (_view == null)
+            {
+                return;
+            }
+
+            if (_view.Model == null)
+            {
+                return;
+            }
+
+            InputManager.Instance.HandleInput(_view.Model.Data);
         }
     }
 }
--- Assets/Scripts/Views/CalculatorView.cs
+++ Assets/Scripts/Views/CalculatorView.cs
@@ -1,4 +1,5 @@
 ﻿using Common;
+using Components.BaseComponents;
 
 namespace Views
 {
