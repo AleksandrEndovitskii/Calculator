c44f426b901dd004b26a2ef979ef686b55b1dfff
Aleksandr Endovitskii <endovitsky.a.p@gmail.com>
2022-07-08 01:56:13 +0400

    Add InputNumberModel

---
+++ Assets/Scripts/Models/InputNumberModel.cs
@@ -0,0 +1,12 @@
+﻿using Common;
+
+namespace Models
+{
+    public class InputNumberModel : BaseModel<int>
+    {
+        public InputNumberModel(int data) : 
+            base(data)
+        {
+        }
+    }
+}
